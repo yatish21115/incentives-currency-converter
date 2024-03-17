46a125a6fde4dcda6ededd08e5ea690140e3c114
yatishsharma <yatish.sharma@goevive.com>
2024-03-17 19:24:17 +0530

    added error handler middleware to the application-level middleware

--- src/middlewares/errorHandler.ts
+++ src/middlewares/errorHandler.ts
@@ -2,7 +2,7 @@ import {Request, Response, NextFunction} from "express";
 import {StatusCodes} from "../utils/StatusCodes";
 
 export const errorHandler = (err: Error, req: Request, res: Response, next: NextFunction) => {
-    console.error(err.stack); // Log the error
+    console.error(err.stack);
     if (!("statusCode" in err)) {
         return res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({message: 'Internal Server Error', error: err.message});
     }
